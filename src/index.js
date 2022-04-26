67acda47be70731bdef8ab2968a461804733905a
Jory Shilmover <jorys5116@bigfoot.spokane.edu>
2022-04-25 20:14:43 -0700

    Week 2 Day 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 function Test() {
-  return <p>This is a test</p>;
+    return <p>This is a test</p>
 }
 
-render(<Test />, document.getElementById("app"));
+render(<Test />, document.getElementById("app"))
