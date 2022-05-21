9ce0c451e0474dc25f92183968c650c1741be962
Jory Shilmover <Jorys5116@bigfoot.spokane.edu>
2022-05-21 16:58:19 -0700

    Got bootstrap tables fully implemented

--- src/components/BidTypeView/BidTypeView.jsx
+++ src/components/BidTypeView/BidTypeView.jsx
@@ -6,34 +6,28 @@ export function BidTypeView({
 }) {
   if (state === "IMPORTING") {
     return (
-      <div className="row importing">
-        <div className="col-1 align-self-center">
-          <p>{id}</p>
-        </div>
-        <div className="col-3 align-self-center">
-          <p>{bidType}</p>
-        </div>
-        <div className="col-2 align-self-center">
-          <p>Importing</p>
-        </div>
-        <div className="col-1 align-self-center">
-          <p>{bidPeriods}</p>
-        </div>
-        <div className="col-3 align-self-center">
-          <p>{lastImport}</p>
-        </div>
-        <div className="row col-2 align-self-center">
-          <button className="btn btn-danger my-2">
-            <span className="bi bi-x" />
-            Cancel
-          </button>
-        </div>
-      </div>
+      <tr className="importing">
+        <th className="col-1 align-middle fw-normal" scope="row">
+          {id}
+        </th>
+        <td className="col-2 align-middle">{bidType}</td>
+        <td className="col-2 align-middle">Importing</td>
+        <td className="col-3 align-middle">{bidPeriods}</td>
+        <td className="col-2 align-middle">{lastImport}</td>
+        <td className="col-2 align-middle">
+          <div>
+            <button className="col-12 btn btn-danger my-2">
+              <span className="bi bi-x" />
+              Cancel
+            </button>
+          </div>
+        </td>
+      </tr>
     );
   }
   return (
     <tr>
-      <th className="col-1 align-middle" scope="row">
+      <th className="col-1 align-middle fw-normal" scope="row">
         {id}
       </th>
       <td className="col-2 align-middle">{bidType}</td>
@@ -60,40 +54,6 @@ export function BidTypeView({
         </div>
       </td>
     </tr>
-    /*     <div className="row">
-      <div className="col-1 align-self-center">
-        <p>{id}</p>
-      </div>
-      <div className="col-3 align-self-center">
-        <p>{bidType}</p>
-      </div>
-      <div className="col-2 align-self-center">
-        <p>Current</p>
-      </div>
-      <div className="col-1 align-self-center">
-        <p>{bidPeriods}</p>
-      </div>
-      <div className="col-3 align-self-center">
-        <p className="text-center">{lastImport}</p>
-      </div>
-      <div className="row col-2 align-self-center">
-        <button className="btn retry-import-button my-1">
-          <span className="bi bi-cloud-download" /> Retry Import
-        </button>
-        <button className="btn secondary-bidtype-button my-1">
-          Bid Periods
-          <span className="bi bi-caret-right-fill" />
-        </button>
-        <button className="btn secondary-bidtype-button my-1">
-          Import History
-          <span className="bi bi-caret-right-fill" />
-        </button>
-        <button className="btn btn-danger my-1">
-          <span className="bi bi-x" />
-          Delete
-        </button>
-      </div>
-    </div> */
   );
 }
 
