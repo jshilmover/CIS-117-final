613be63b4e40a909f12989688750030f424b17d6
jory shilmover <joryshilmover@gmail.com>
2022-05-29 21:53:25 +0000

    Revert "Intro to Redux Part 2"

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -37,7 +37,7 @@ module.exports = {
         use: ["babel-loader", "eslint-loader"],
       },
       {
-        test: /(\.css)$/i,
+        test: /(\.css)$/,
         use: ["style-loader", "css-loader"],
       },
       {
