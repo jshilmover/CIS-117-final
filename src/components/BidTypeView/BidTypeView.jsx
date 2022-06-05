ba5682184963d368870805aeec67995e32dd20ed
Jory Shilmover <jorys5116@bigfoot.spokane.edu>
2022-06-05 13:45:39 -0700

    Converted from table to flex

--- src/components/BidTypeView/BidTypeView.jsx
+++ src/components/BidTypeView/BidTypeView.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /**
- * 
+ *
  * View component that displays one bid type.
- * 
+ *
  */
 
 export function BidTypeView({
@@ -13,6 +13,62 @@ export function BidTypeView({
 }) {
   if (state === "IMPORTING") {
     return (
+      <div className="row align-items-center importing">
+        <hr className="bids-seperator" />
+        <div className="col-1">{id}</div>
+        <div className="col-2 text-center">{bidType}</div>
+        <div className="col-3 text-center">Current</div>
+        <div className="col-2">{bidPeriods}</div>
+        <div className="col-2">{lastImport}</div>
+        <div className="col-2">
+          <button className="col-12 btn btn-danger my-2">
+            <FontAwesomeIcon icon="fa-solid fa-xmark" /> Cancel
+          </button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="row align-items-center bid-table">
+      <hr className="bids-seperator" />
+      <div className="col-1">{id}</div>
+      <div className="col-2">{bidType}</div>
+      <div className="col-3 text-center">Current</div>
+      <div className="col-2 text-center">{bidPeriods}</div>
+      <div className="col-2">{lastImport}</div>
+      <div className="col-2">
+        <button className="col-12 btn retry-import-button my-1">
+          <FontAwesomeIcon icon="fa-solid fa-cloud-arrow-down" /> Retry Import
+        </button>
+        <button className=" col-12 btn secondary-bidtype-button my-1">
+          Bid Periods <FontAwesomeIcon icon="fa-solid fa-angles-right" />
+        </button>
+        <button className="col-12 btn secondary-bidtype-button my-1">
+          Import History <FontAwesomeIcon icon="fa-solid fa-angles-right" />
+        </button>
+        <button className="col-12 btn btn-danger my-1">
+          <FontAwesomeIcon icon="fa-solid fa-xmark" /> Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+BidTypeView.propTypes = {
+  //composition of the bidtype
+  bidTypes: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    bidType: PropTypes.string.isRequired,
+    bidPeriods: PropTypes.string.isRequired,
+    lastImport: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+};
+
+/**
+ * OLD TABLE WAY FOR IMPORTING
+ * 
+ * return (
       <tr className="importing">
         <th className="col-1 align-middle fw-normal" scope="row">
           {id}
@@ -30,9 +86,12 @@ export function BidTypeView({
         </td>
       </tr>
     );
-  }
-  return (
-    <tr>
+ */
+
+/**
+   * OLD TABLE WAY FOR NORMAL VIEW
+   * 
+   *     <tr>
       <th className="col-1 align-middle fw-normal" scope="row">
         {id}
       </th>
@@ -57,16 +116,5 @@ export function BidTypeView({
         </div>
       </td>
     </tr>
-  );
-}
-
-BidTypeView.propTypes = {
-  //composition of the bidtype
-  bidTypes: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    bidType: PropTypes.string.isRequired,
-    bidPeriods: PropTypes.string.isRequired,
-    lastImport: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
-  }),
-};
+   * 
+   */
